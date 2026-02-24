3b8713eef4b62663426ea3d0c6c50283444d08dd
Andy <andycann44@yahoo.com>
2026-02-24 12:37:38 +0000

    css/ios: fix safari flash and keep capacitor bundled config

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -4,11 +4,10 @@ const config: CapacitorConfig = {
   appId: "uk.co.aim2build.app",
   appName: "Aim2Build",
   webDir: "dist",
-  server: {
-  url: "https://aim2build.co.uk",
-  cleartext: false,
-  allowNavigation: ["aim2build.co.uk", "*.aim2build.co.uk"]
-  }
+ // server: {
+ // cleartext: false,
+ // allowNavigation: ["aim2build.co.uk", "*.aim2build.co.uk"]
+ // }
 };
 
 export default config;
